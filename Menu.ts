925e12e66d95480da6a816aac35e3f8165cbfe4c
user <user@users-MacBook-Pro.local>
2025-11-10 14:10:00 -0300

    Inicial

--- Menu.ts
+++ Menu.ts
@@ -2,6 +2,7 @@ import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
 import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 export function main() {
 
@@ -22,6 +23,14 @@ export function main() {
     contacorrente.visualizar();
     contacorrente.depositar(1000);
     contacorrente.visualizar();
+
+    // Objeto da Classe ContaPoupanca (teste)
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
 
     while (true) {
 
